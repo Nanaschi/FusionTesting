1f3a7ac630a3cd2801c6fa3c6014875d88bfef90
Daniel <naturforscher@outlook.com>
2022-10-31 19:52:06 +0200

    Implemented the ball spawn 103- prediction

--- Assets/VanillaTutorial/Scripts/BasicSetUp/BasicSpawner.cs
+++ Assets/VanillaTutorial/Scripts/BasicSetUp/BasicSpawner.cs
@@ -12,26 +12,27 @@ namespace VanillaTutorial
     {
         [SerializeField] private NetworkPrefabRef _playerPrefab;
         private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new();
-        
+        private bool _mouseButton0;
         private NetworkRunner _runner;
 
         private void OnGUI()
         {
             if (_runner == null)
             {
-                if (GUI.Button(new Rect(0,0,200,40), "Host"))
+                if (GUI.Button(new Rect(0, 0, 200, 40), "Host"))
                 {
                     StartGame(GameMode.Host);
                     print(nameof(GameMode.Host));
                 }
-                if (GUI.Button(new Rect(0,40,200,40), "Join"))
+
+                if (GUI.Button(new Rect(0, 40, 200, 40), "Join"))
                 {
                     StartGame(GameMode.Client);
                     print(nameof(GameMode.Client));
                 }
             }
         }
-        
+
         async void StartGame(GameMode mode)
         {
             // Create the Fusion runner and let it know that we will be providing user input
@@ -45,18 +46,23 @@ namespace VanillaTutorial
                 SessionName = "TestRoom",
                 Scene = SceneManager.GetActiveScene().buildIndex,
                 SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
-            });}
-        
+            });
+        }
 
 
-        
+        private void Update()
+        {
+            _mouseButton0 = _mouseButton0 | Input.GetMouseButton(0);
+        }
+
+
         public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
         {
             if (runner.IsServer)
             {
                 print(player.RawEncoded);
                 // Create a unique position for the player
-                Vector3 spawnPosition = new Vector3((player.RawEncoded%runner.Config.Simulation.DefaultPlayers)*3,1,0);
+                Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.DefaultPlayers) * 3, 1, 0);
                 NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
                 // Keep track of the player avatars so we can remove it when they disconnect
                 _spawnedCharacters.Add(player, networkPlayerObject);
@@ -89,76 +95,64 @@ namespace VanillaTutorial
             if (Input.GetKey(KeyCode.D))
                 data.direction += Vector3.right;
 
+
+            if (_mouseButton0) data.buttons |= NetworkInputData.MOUSEBUTTON1;
+            _mouseButton0 = false;
+            
             print(MethodBase.GetCurrentMethod());
             input.Set(data);
         }
 
         public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
         {
-            
         }
 
         public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
         {
-            
         }
 
         public void OnConnectedToServer(NetworkRunner runner)
         {
-            
         }
 
         public void OnDisconnectedFromServer(NetworkRunner runner)
         {
-            
         }
 
         public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
         {
-            
         }
 
         public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
         {
-            
         }
 
         public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
         {
-            
         }
 
         public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
         {
-            
         }
 
         public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
         {
-            
         }
 
         public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
         {
-            
         }
 
         public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
         {
-            
         }
 
         public void OnSceneLoadDone(NetworkRunner runner)
         {
-            
         }
 
         public void OnSceneLoadStart(NetworkRunner runner)
         {
-            
         }
-    
-    
-    
     }
 }
--- Assets/VanillaTutorial/Scripts/BasicSetUp/NetworkInputData.cs
+++ Assets/VanillaTutorial/Scripts/BasicSetUp/NetworkInputData.cs
@@ -5,6 +5,9 @@ namespace VanillaTutorial
 {
     public struct NetworkInputData: INetworkInput
     {
+        public const byte MOUSEBUTTON1 = 0x01;
+
+        public byte buttons;
         public Vector3 direction;
     }
 }
